a5b7466259186148c69f81ba84345d4a56c5a7c7
VanHC <VanHC@VANHC>
2018-10-22 17:30:06 +0700

    commit add book and upload image

--- LibraryManagement/Controllers/BookController.cs
+++ LibraryManagement/Controllers/BookController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LibraryManagement.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,10 +13,64 @@ namespace LibraryManagement.Controllers
         // GET: Book
         public ActionResult Index()
         {
+           
+
             return View();
         }
+        
         public ActionResult AddBook()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadImage()
+        {
+            List<HttpPostedFileBase> allFiles = Enumerable.Range(0, Request.Files.Count)
+                                                .Select(x => Request.Files[x])
+                                                .Where(x => !string.IsNullOrEmpty(x.FileName))
+                                                .ToList();
+            TempData["ListImageUpload"] = allFiles;
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult AddBook(Book book)
+        {
+            IEnumerable<HttpPostedFileBase> files = TempData["ListImageUpload"] as IEnumerable<HttpPostedFileBase>;
+            try
+            {
+                using (var ctx = new LibraryManagementEntities())
+                {
+                    book.ImportDate = DateTime.Now;
+                    book.Flag = true;
+                    ctx.Books.Add(book);
+                    ctx.SaveChanges();
+                    string auDirPath = Server.MapPath("~/Public/BookImages");
+                    string targetDirPath = Path.Combine(auDirPath, book.ID.ToString());
+                    Directory.CreateDirectory(targetDirPath);
+                    List<string> fileNames = new List<string>();
+                    int i = 0;
+                    foreach (var file in files)
+                    {
+                        if (file.ContentLength > 0 && file != null)
+                        {
+                            string pathOfFile = Path.Combine(targetDirPath, $"{i}.jpg");
+                            string linkToFile = $"/Public/BookImages/{i}.jpg";
+                            fileNames.Add(linkToFile);
+                            file.SaveAs(pathOfFile);
+                            i++;
+                        }
+                    }
+                    book.Image = String.Join(";", fileNames);
+                    ctx.Entry(book).State = System.Data.Entity.EntityState.Modified;
+                    ctx.SaveChanges();
+                    ViewBag.msgSuccess = "Nhập sách thành công!";
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.msgError = ex.Message;
+            }
             return View();
         }
     }
